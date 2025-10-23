1ff051bb70c3a11f7d4026d82ef82df530918bfe
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-10-23 12:24:41 +0530

    update systemconfig-manager

---
+++ .config/sysconfig/custom-commands.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Custom commands to run during system restore
+# These commands will be executed after system update but before package installation
+# 
+# Examples:
+# echo "Running custom setup..."
+# mkdir -p ~/.local/bin
+# curl -o ~/.local/bin/some-tool https://example.com/tool
+# chmod +x ~/.local/bin/some-tool
+
+# Add your custom commands below:
+
